c03cf3aa87e05cb614c715c7af1cad4c114eb5c0
gary.ma <garymar2007@gmail.com>
2024-07-06 11:23:12 +0200

    terraform formatting.

--- main.tf
+++ main.tf
@@ -37,11 +37,20 @@ resource "aws_route_table" "gary_public_rt" {
 resource "aws_route" "gary_route" {
   route_table_id         = aws_route_table.gary_public_rt.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.gary_igw.id
-  depends_on = [aws_internet_gateway.gary_igw] 
+  gateway_id             = aws_internet_gateway.gary_igw.id
+  depends_on             = [aws_internet_gateway.gary_igw]
 }
 
 resource "aws_route_table_association" "gary_public_subnet_association" {
   subnet_id      = aws_subnet.gary_public_subnet.id
   route_table_id = aws_route_table.gary_public_rt.id
 }
+
+# resource "aws_security_group" "gary_sg" {
+#   vpc_id = aws_vpc.gary_vpc.id
+
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["
--- providers.tf
+++ providers.tf
@@ -8,5 +8,5 @@ terraform {
 
 #Configure the AWS provider
 provider "aws" {
-  region                   = "us-east-2"
+  region = "us-east-2"
 }
